36859b19bd12cba9bf7374c9160379aa05945462
João Melo <jopcmelo@gmail.com>
2019-12-04 00:05:23 -0300

    feat: Add store data when data is an object

--- src/app/controllers/EndpointController.js
+++ src/app/controllers/EndpointController.js
@@ -1,7 +1,5 @@
-import Endpoint from '../models/Endpoint';
 import EndpointField from '../models/EndpointsField';
 import EndpointFieldValue from '../models/EndpointsFieldValue';
-import EndpointOrder from '../models/EndpointsOrder';
 
 class EndpointController {
   async store(req, res) {
@@ -12,6 +10,15 @@ class EndpointController {
         endpoint_id: req.endpointId,
       },
       attributes: ['id', 'title', 'created_at', 'updated_at'],
+      include: [
+        {
+          model: EndpointFieldValue,
+          as: 'values',
+          where: {
+            user_id: req.userId,
+          },
+        },
+      ],
     });
 
     if (fields.length > 1) {
@@ -20,8 +27,35 @@ class EndpointController {
     } else if (typeof data !== 'string')
       return res.status(400).json({ error: 'Data object malformed' });
 
+    // if data received from body is an object, means that we need to insert more than one field value into database
     if (fields.length > 1) {
-      // ...
+      fields.map(field => {
+        if (!data[field.getDataValue('title')]) {
+          return res.status(400).json({
+            error: `Please, provide ${field.getDataValue('title')} field`,
+          });
+        }
+
+        return field;
+      });
+
+      Promise.all(
+        fields.map(field => {
+          // TODO - Add validation for check if the current value is the same
+
+          if (
+            field.getDataValue('value') !== data[field.getDataValue('title')]
+          ) {
+            return EndpointFieldValue.create({
+              endpoints_field_id: field.getDataValue('id'),
+              value: data[field.getDataValue('title')],
+              user_id: req.userId,
+            });
+          }
+
+          return field;
+        })
+      );
     } else {
       const fieldValue = await EndpointFieldValue.findOne({
         where: {
