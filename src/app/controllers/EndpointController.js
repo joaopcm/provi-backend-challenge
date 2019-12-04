3fccc1e203f926b75fc8f035ea145f2f5014cd3d
João Melo <jopcmelo@gmail.com>
2019-12-03 23:21:26 -0300

    chore: Add update field value logic

--- src/app/controllers/EndpointController.js
+++ src/app/controllers/EndpointController.js
@@ -8,45 +8,68 @@ class EndpointController {
     const { slug } = req.params;
     const { data } = req.body;
 
-    const endpoint = await Endpoint.findOne({
-      attributes: ['id', 'slug'],
-      include: [
-        {
-          model: EndpointField,
-          as: 'fields',
-          attributes: ['id', 'title'],
-          include: [
-            {
-              model: EndpointFieldValue,
-              as: 'values',
-              order: [['created_at', 'desc']],
-              limit: 1,
-              where: {
-                user_id: req.userId,
-              },
-            },
-          ],
-        },
-        {
-          model: EndpointOrder,
-          as: 'order',
-          attributes: ['id', 'order'],
-        },
-      ],
-      where: { slug },
+    // const endpoint = await Endpoint.findByPk(req.endpointId, {
+    //   attributes: ['id', 'slug'],
+    //   include: [
+    //     {
+    //       model: EndpointField,
+    //       as: 'fields',
+    //       attributes: ['id', 'title'],
+    //       include: [
+    //         {
+    //           model: EndpointFieldValue,
+    //           as: 'values',
+    //           order: [['created_at', 'desc']],
+    //           limit: 1,
+    //           where: {
+    //             user_id: req.userId,
+    //           },
+    //           attributes: ['value', 'updated_at'],
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       model: EndpointOrder,
+    //       as: 'order',
+    //       attributes: ['order'],
+    //     },
+    //   ],
+    // });
+
+    const fields = await EndpointField.findAll({
+      where: {
+        endpoint_id: req.endpointId,
+      },
+      attributes: ['id', 'title', 'created_at', 'updated_at'],
     });
 
-    if (typeof data === 'object') {
+    if (fields.length > 1) {
+      if (typeof data !== 'object')
+        return res.status(400).json({ error: 'Data object malformed' });
+    } else if (typeof data !== 'string')
+      return res.status(400).json({ error: 'Data object malformed' });
+
+    if (fields.length > 1) {
       // ...
     } else {
-      // await EndpointFieldValue.create({
-      //   user_id: req.userId,
-      //   endpoints_field_id: endpoint.getDataValue('fields')[0].id,
-      //   value: data,
-      // });
+      const fieldValue = await EndpointFieldValue.findOne({
+        where: {
+          endpoints_field_id: fields[0].getDataValue('id'),
+          user_id: req.userId,
+        },
+        order: [['created_at', 'desc']],
+      });
+
+      if (!fieldValue || fieldValue.getDataValue('value') !== data) {
+        await EndpointFieldValue.create({
+          endpoints_field_id: fields[0].getDataValue('id'),
+          value: data,
+          user_id: req.userId,
+        });
+      }
     }
 
-    return res.json({ ok: 'ok' });
+    return res.json(fields);
   }
 }
 
